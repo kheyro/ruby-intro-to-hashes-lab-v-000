9e4fc90bd766805f08d7e25e4f37662d9d3bf7ee
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-30 20:56:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	{ railroads: {names: {} }}
+	{ railroads: {
+			names: {},
+			rent_in_dollars: {}
+		}
+	 }
 end
 
 def monopoly_with_fourth_tier
